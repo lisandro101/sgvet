624e34a7d918a146f99f803482147470e0146de8
stafoxter <stafoxter@a71d8cfa-91c4-11de-82e4-47d6b490cfbe>
2009-09-30 20:51:47 +0000

    Corregi el problema de los periodos. Ahora se manejan 13 periodos con fecha de inicio y fin relativos al año. Agregue poder modificar la fecha actual con la q trabaja el sistema. Esta en el menu "Configuracion" Corregi el problema de cierre de periodos Otras correcciones relacionadas a las anteriores y tb generales

--- trunk/src/sgvet/entidades/DemandaXPeriodo.java
+++ trunk/src/sgvet/entidades/DemandaXPeriodo.java
@@ -12,6 +12,12 @@ import java.util.Date;
  * @author Seba
  */
 public class DemandaXPeriodo {
+    private int nroPeriodo; // son 13 perioodos- del 1 al 13
+    private Date anio;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private boolean cerrado; // false si el periodo esta abierto (POR DEFECTO), true si esta cerrado
+
     private Date mes;
     private int totalVentas;
     private int prediccionVenta;
@@ -21,6 +27,9 @@ public class DemandaXPeriodo {
         totalVentas= total;
     }
 
+    public DemandaXPeriodo() {
+
+    }
     /**
      * @return the mes
      */
@@ -61,6 +70,76 @@ public class DemandaXPeriodo {
      */
     public void setPrediccionVenta(int prediccionVenta) {
         this.prediccionVenta = prediccionVenta;
+    }
+
+    /**
+     * @return the nroPeriodo
+     */
+    public int getNroPeriodo() {
+        return nroPeriodo;
+    }
+
+    /**
+     * @param nroPeriodo the nroPeriodo to set
+     */
+    public void setNroPeriodo(int nroPeriodo) {
+        this.nroPeriodo = nroPeriodo;
+    }
+
+    /**
+     * @return the anio
+     */
+    public Date getAnio() {
+        return anio;
+    }
+
+    /**
+     * @param anio the anio to set
+     */
+    public void setAnio(Date anio) {
+        this.anio = anio;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the cerrado
+     */
+    public boolean isCerrado() {
+        return cerrado;
+    }
+
+    /**
+     * @param cerrado the cerrado to set
+     */
+    public void setCerrado(boolean cerrado) {
+        this.cerrado = cerrado;
     }
 
     
